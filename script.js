464ee6604f439511c173dd3834ac4978d6269079
datrolld20 <datrolld20@gmail.com>
2020-09-14 22:02:59 -0400

    Finished header

--- script.js
+++ script.js
@@ -16,3 +16,13 @@ function clearInput() {
     searchBar.value = "";
     xBtn.style.visibility = 'hidden';
 }
+
+function navLinkSelect(e) {
+    let navLinks = document.getElementsByClassName("nav-link");
+
+    for(let i = 0; i < navLinks.length; i++) {
+        navLinks[i].classList.remove("current");
+    }
+
+    e.classList.add("current");
+}
